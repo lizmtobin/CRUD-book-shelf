9e0a30cdfb64c998d1fac0ab7f905204f99012e3
liz tobin <lizmtobin@gmail.com>
2021-10-15 11:20:36 +0100

    update book integratin test passed

--- src/test/java/com/qa/bookshelf/rest/BookIntegrationTest.java
+++ src/test/java/com/qa/bookshelf/rest/BookIntegrationTest.java
@@ -1,7 +1,9 @@
 package com.qa.bookshelf.rest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -78,4 +80,34 @@ public class BookIntegrationTest {
 
 		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkContent);
 	}
+
+	@Test
+	void testUpdate() throws Exception {
+		final Book testBook = new Book(1, "Design Patterns", "Freeman", 300, "Educational");
+		final String testBookAsJSON = this.mapper.writeValueAsString(testBook);
+
+		RequestBuilder request = put("/book/update/1").contentType(MediaType.APPLICATION_JSON).content(testBookAsJSON);
+
+		ResultMatcher checkStatus = status().isAccepted();
+		ResultMatcher checkContent = content().json(testBookAsJSON);
+
+		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkContent);
+	}
+
+	@Test
+	void testDelete() throws Exception {
+		this.mvc.perform(delete("/book/remove/1")).andExpect(status().isNoContent());
+	}
+
+	void testCreateAbrdiged() throws Exception {
+		final String testBookAsJSON = this.mapper
+				.writeValueAsString(new Book(null, "Design Patterns", "Freeman", 300, "Educational"));
+		final String savedBookAsJSON = this.mapper
+				.writeValueAsString(new Book(1, "Design Patterns", "Freeman", 300, "Educational"));
+
+		this.mvc.perform(post("/book/create").contentType(MediaType.APPLICATION_JSON).content(testMarsupialAsJSON))
+				.andExpect(status().isCreated()).andExpect(content().json(savedMarsupialAsJSON));
+
+	}
+
 }
