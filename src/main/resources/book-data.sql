fe1220b42da957e36050f5e6af6c4c1dc6b864a5
liz tobin <lizmtobin@gmail.com>
2021-10-15 11:06:55 +0100

    get by id integration test passed - built sql schema and data file to handle running order problems

---
+++ src/main/resources/book-data.sql
@@ -0,0 +1,4 @@
+insert into book
+	(title, author, pages, genre)
+		values
+	('Design Patterns', 'Freeman', 300, 'Educational');
--- src/test/java/com/qa/bookshelf/rest/BookIntegrationTest.java
+++ src/test/java/com/qa/bookshelf/rest/BookIntegrationTest.java
@@ -13,6 +13,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -22,6 +24,9 @@ import com.qa.bookshelf.data.Book;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Sql(scripts = { "classpath:book-schema.sql",
+		"classpath:book-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+
 public class BookIntegrationTest {
 
 	@Autowired
@@ -36,7 +41,7 @@ public class BookIntegrationTest {
 		final Book testBook = new Book(null, "title", "author", null, "genre");
 		String testBookAsJSON = this.mapper.writeValueAsString(testBook);
 
-		final Book savedBook = new Book(1, "title", "author", null, "genre");
+		final Book savedBook = new Book(2, "title", "author", null, "genre");
 		String savedBookAsJSON = this.mapper.writeValueAsString(savedBook);
 
 		RequestBuilder request = post("/book/create").contentType(MediaType.APPLICATION_JSON).content(testBookAsJSON);
@@ -50,9 +55,23 @@ public class BookIntegrationTest {
 
 	@Test
 	void testGetAll() throws Exception {
-		String savedBookAsJSON = this.mapper.writeValueAsString(List.of(new Book(1, "title", "author", null, "genre")));
+		String savedBookAsJSON = this.mapper
+				.writeValueAsString(List.of(new Book(1, "Design Patterns", "Freeman", 300, "Educational")));
 
 		RequestBuilder request = get("/book/books");
+
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkContent = content().json(savedBookAsJSON);
+
+		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkContent);
+	}
+
+	@Test
+	void testGetById() throws Exception {
+		final Book savedBook = new Book(1, "Design Patterns", "Freeman", 300, "Educational");
+		String savedBookAsJSON = this.mapper.writeValueAsString(savedBook);
+
+		RequestBuilder request = get("/book/get/" + savedBook.getId());
 
 		ResultMatcher checkStatus = status().isOk();
 		ResultMatcher checkContent = content().json(savedBookAsJSON);
